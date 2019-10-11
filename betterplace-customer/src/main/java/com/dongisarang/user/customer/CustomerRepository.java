28f685d1f618b31fba23d65cbe8430d2bc5c06ad
Vincent <gunflake09@gmail.com>
2019-10-12 00:48:00 +0900

    회원정보 수정

--- betterplace-customer/src/main/java/com/dongisarang/user/customer/CustomerRepository.java
+++ betterplace-customer/src/main/java/com/dongisarang/user/customer/CustomerRepository.java
@@ -2,6 +2,10 @@ package com.dongisarang.user.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     Customer findByCustomerIdAndCustomerPassword(String customerId, String customerPassword);
+    Customer findByCustomerId(String customerId);
+
 }
