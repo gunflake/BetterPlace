57871477d82cdbc12246d5ba36f1fe01969408dc
HSKim <bfpaul@daum.net>
2019-11-14 18:03:44 +0900

    SideNav 적용

--- betterplace-customer/src/main/java/com/dongisarang/user/security/SecurityConfig.java
+++ betterplace-customer/src/main/java/com/dongisarang/user/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.GrantedAuthority;
@@ -26,6 +27,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     CustomerRepository customerRepository;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/script/**", "/image/**", "/fonts/**", "/lib/**");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
