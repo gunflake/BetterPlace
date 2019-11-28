c69edcf31b7729243fec29bd967bf8538f6a8ec1
Kim-Wonkyeom <yonkum@naver.com>
2019-11-28 22:49:20 +0900

    병합중

---
+++ betterplace-partner/src/main/java/com/dongisarang/partner/PartnerApplication.java
@@ -0,0 +1,14 @@
+package com.dongisarang.partner;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PartnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartnerApplication.class, args);
+    }
+}
---
+++ betterplace-partner/src/main/java/com/dongisarang/partner/place/Place.java
@@ -0,0 +1,62 @@
+package com.dongisarang.partner.place;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+public class Place {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer placeNo; // 공간번호
+
+    @Column
+    private String placeName; // 공간명
+
+    @Column
+    private String intro; // 공간 소개
+
+    @Column
+    private String tag; // 태그
+
+    @Column
+    private String info; // 시설 안내
+
+    @Column
+    private String convenience; // 편의시설
+
+    @Column
+    private String notice; // 예약 시 주의사항
+
+    @Column
+    private String address; // 주소
+
+    @Column
+    private String website; // 웹사이트
+
+    @Column
+    private String email; // 이메일
+
+    @Column
+    private String phone; // 대표전화번호
+
+    @CreationTimestamp
+    private Date registerDate; //등록일
+
+    @UpdateTimestamp
+    private Date updateDate; //수정일
+
+    @Column
+    private String image; // 대표이미지
+
+    @Column
+    private Byte state; //상태
+
+}
