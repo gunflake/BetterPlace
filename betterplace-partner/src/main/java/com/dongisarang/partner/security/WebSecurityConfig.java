c362569669f00648a9696abc3feeab984986d684
Vincent <gunflake09@gmail.com>
2019-12-07 13:27:25 +0900

    사이드바 구현 및 해더 구현 완료 (공지사항 페이지 구현해야함..)

--- betterplace-partner/src/main/java/com/dongisarang/partner/security/WebSecurityConfig.java
+++ betterplace-partner/src/main/java/com/dongisarang/partner/security/WebSecurityConfig.java
@@ -42,7 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        //web.ignoring().mvcMatchers("/templates/**");
+        web.ignoring().mvcMatchers("/templates/**");
         web.ignoring().antMatchers("/css/**", "/script/**", "/image/**", "/fonts/**", "/lib/**", "/js/**");
     }
 
