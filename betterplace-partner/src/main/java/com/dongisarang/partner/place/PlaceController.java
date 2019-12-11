07d2d682a794e3d1eded0b05785bdfd661d54228
haramnoh <un2577@gmail.com>
2019-12-11 23:33:57 +0900

    공간 등록 추가 (Place)

--- betterplace-partner/src/main/java/com/dongisarang/partner/place/PlaceController.java
+++ betterplace-partner/src/main/java/com/dongisarang/partner/place/PlaceController.java
@@ -3,9 +3,11 @@ package com.dongisarang.partner.place;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Log
@@ -18,25 +20,29 @@ public class PlaceController {
     @Autowired
     PlaceDtlRepository placeDtlRepository;
 
+    @Autowired
+    PlaceService placeService;
+
     //@Autowired
     //PlaceService placeService;
 
     /* 공간 등록페이지 */
     @GetMapping("/place/registration")
     public String initPlaceRegistForm(){
-
+        /*
         Place place = placeRepository.findPlaceByPlaceNo(1);
 
         if(place != null)
             log.info(""+place);
 
         // 출력
+
         List<PlaceDtl> placeDtlList = place.getPlaceDtls();
         if(!placeDtlList.isEmpty())
             placeDtlList.forEach(placeDtl -> {
             System.out.println(placeDtl);
         });
-
+            */
 
         // 등록
         //PlaceDtl placeDtl = new PlaceDtl(place,"test11", "test11", 22, 22);
@@ -47,7 +53,17 @@ public class PlaceController {
         return "page/place_registration";
     }
 
-
+    /* 공간 등록 후 메인으로 이동한다.*/
+    @PostMapping("/place/registration")
+    public String processPlaceRegistration(Place place, BindingResult result){
+        //TODO: 유효성 추가
+        if(result.hasErrors()){
+            return "/";
+        }else{
+            placeService.createPlace(place);
+            return "redirect:/";
+        }
+    }
 
 
 }
--- betterplace-partner/src/main/java/com/dongisarang/partner/place/PlaceService.java
+++ betterplace-partner/src/main/java/com/dongisarang/partner/place/PlaceService.java
@@ -1,11 +1,18 @@
 package com.dongisarang.partner.place;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PlaceService {
     @Autowired
     PlaceRepository placeRepository;
     //public Place findPlace(Integer placeno){
         //return placeRepository.findByPlaceNo(placeno);
     //}
+
+    /* 장소 등록 */
+    public void createPlace(Place place) {
+        placeRepository.save(place);
+    }
 }
